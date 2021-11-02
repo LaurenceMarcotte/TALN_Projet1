206defe0b649854af1fb0600c48594a2455c2569
Naima Hassert <naima.hassert@gmail.com>
2021-11-01 22:52:48 -0400

    Modèle CoLA de base.

--- cola_model.py
+++ cola_model.py
@@ -1,27 +1,17 @@
-import scipy
+# %%
 import sys
 import argparse
-from numpy.lib.function_base import vectorize
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import sklearn
-from sklearn.neural_network import MLPClassifier, MLPRegressor
+from sklearn.neural_network import MLPClassifier
 
-from gensim.test.utils import datapath, temporary_file
-from gensim import utils
-import gensim.models
-import tempfile
+import gensim
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from math import sqrt
 from sklearn.metrics import classification_report, confusion_matrix
 
 # code inspiré de https://www.pluralsight.com/guides/machine-learning-neural-networks-scikit-learn
 
 # %%
-# rendre ça clean éventuellement avec un argparse
 
 
 def make_parser():
@@ -30,11 +20,6 @@ def make_parser():
     parser.add_argument('--dev_file', type=str)
     parser.add_argument('--dataset_name', type=str)
     return parser
-
-
-train_file = r"C:\Users\Naima\OneDrive - Universite de Montreal\TALN\Projet 1\data\cola_public_1.1\cola_public\tokenized\in_domain_train.tsv"
-dev_file = r"C:\Users\Naima\OneDrive - Universite de Montreal\TALN\Projet 1\data\cola_public_1.1\cola_public\tokenized\in_domain_dev.tsv"
-dataset_name = "cola"
 
 
 def read_data(file_name: str, dataset_name: str) -> pd.DataFrame:
@@ -59,27 +44,14 @@ def read_data(file_name: str, dataset_name: str) -> pd.DataFrame:
         data['sentence'] = data.apply(
             lambda row: row['question1'] + ' ' + row['question2'], axis=1)
         data = data.rename(columns={'is_duplicate': 'label'})
-    elif dataset_name == 'sst':
-        # TODO: Renommer les colonnes du dataframe contenant les phrases et les classes en "sentence" et "label" pour
-        #  sst
-        pass
 
     return data
-
-
-# donner en input des représentations word2vec
-train_data = read_data(train_file, dataset_name)
-test_data = read_data(dev_file, dataset_name)
 
 
 def read_corpus(data):
     for i, line in enumerate(data["sentence"]):
         tokens = gensim.utils.simple_preprocess(line)
         yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
-
-
-train_corpus = list(read_corpus(train_data))
-test_corpus = list(read_corpus(test_data))
 
 
 def convert_to_matrix(corpus, data):
@@ -95,25 +67,45 @@ def convert_to_matrix(corpus, data):
     return stv
 
 
-# X = sentence
-# y = label
-X_train = convert_to_matrix(train_corpus, train_data)
-y_train = train_data["label"].to_numpy()
-X_test = convert_to_matrix(test_corpus, test_data)
-y_test = test_data["label"].to_numpy()
+if __name__ == "__main__":
+    parser = make_parser()
 
-# combien de couches cachées?
-# fit le model aux données de train
-mlp = MLPClassifier(max_iter=1000)
-mlp.fit(X_train, y_train.data)
+    args = parser.parse_args(sys.argv[1:])
 
-predict_train = mlp.predict(X_train)
-predict_test = mlp.predict(X_test)
+    train_file = args.train_file
+    dev_file = args.dev_file
+    dataset_name = args.dataset_name
 
-print(confusion_matrix(y_train, predict_train))
-print(classification_report(y_train, predict_train))
-print("###################################")
-print(confusion_matrix(y_test, predict_test))
-print(classification_report(y_test, predict_test))
+    print("Reading data, preprocessing...")
+
+    # donner en input des représentations word2vec
+    train_data = read_data(train_file, dataset_name)
+    test_data = read_data(dev_file, dataset_name)
+
+    train_corpus = list(read_corpus(train_data))
+    test_corpus = list(read_corpus(test_data))
+
+    # X = sentence
+    # y = label
+    X_train = convert_to_matrix(train_corpus, train_data)
+    y_train = train_data["label"].to_numpy()
+    X_test = convert_to_matrix(test_corpus, test_data)
+    y_test = test_data["label"].to_numpy()
+
+    print("Training model...")
+
+    # fit le model aux données de train
+    mlp = MLPClassifier(max_iter=1000)
+    mlp.fit(X_train, y_train.data)
+
+    predict_train = mlp.predict(X_train)
+    predict_test = mlp.predict(X_test)
+
+    # print les résultats
+    print(confusion_matrix(y_train, predict_train))
+    print(classification_report(y_train, predict_train))
+    print("###################################")
+    print(confusion_matrix(y_test, predict_test))
+    print(classification_report(y_test, predict_test))
 
 # %%
